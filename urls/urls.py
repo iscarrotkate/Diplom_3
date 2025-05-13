2461cb99a91615b239fcbe0cdf4c5c3206dc95ec
Ekaterina Bar <carrot.kate@yandex.ru>
2025-05-13 23:27:53 +0300

    tests added

--- urls/urls.py
+++
@@ -1,8 +0,0 @@
-BASE_URL = 'https://stellarburgers.nomoreparties.site'
-
-LOGIN_URL = '/login'
-PASSWORD_RECOVERY_URL = '/forgot-password'
-PASSWORD_RESET_URL = '/reset-password'
-PROFILE_URL = '/account/profile'
-ORDER_HISTORY_URL = '/account/order-history'
-ORDERS_FEED = '/feed'
